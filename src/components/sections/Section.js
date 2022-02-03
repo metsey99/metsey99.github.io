52bffabd4f34c0b0c58f002398e27ac05ef7420a
Metehan Seyran <seyran17@itu.edu.tr>
2022-02-03 21:18:45 +0300

    Updates

--- src/components/sections/Section.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const ScrollToButton = React.forwardRef(({ id, children }, sectionRef) => (
-  <section ref={sectionRef} id={id}>
-    {children}
-  </section>
-));
-
-export default ScrollToButton;
