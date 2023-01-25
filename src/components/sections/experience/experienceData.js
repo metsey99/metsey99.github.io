c8eb2129676c6df305df1dddb6b088a0899f63da
Metehan Seyran <metsey@Metehans-MacBook-Pro.local>
2023-01-25 16:19:13 +0200

    Update work experience

--- src/components/sections/experience/experienceData.js
+++ src/components/sections/experience/experienceData.js
@@ -1,23 +1,26 @@
-import Commencis from '../../../assets/commencis.png';
-import Definex from '../../../assets/definex.png';
-import Itu from '../../../assets/itu.png';
-import ItuAcm from '../../../assets/ituacm.png';
-import Inzva from '../../../assets/inzva.png';
+import Commencis from "../../../assets/commencis.png";
+import Definex from "../../../assets/definex.png";
+import Itu from "../../../assets/itu.png";
+import ItuAcm from "../../../assets/ituacm.png";
+import Inzva from "../../../assets/inzva.png";
+import Amazon from "../../../assets/amazon.png";
 
 export const experiences = [
   {
     date: "July 2019 - August 2019",
     title: "Research Intern",
     subtitle: "ITU Vision Laboratory",
-    description: "The aim of internship was making familiarize with the concepts of Deep Learning, mainly in Computer Vision area. During the internship, a sign language alphabet classifier using PyTorch has been made.",
+    description:
+      "The aim of internship was making familiarize with the concepts of Deep Learning, mainly in Computer Vision area. During the internship, a sign language alphabet classifier using PyTorch has been made.",
     companyLink: "https://vision.itu.edu.tr/",
-    image: Itu
+    image: Itu,
   },
   {
     date: "July 2020 - August 2020",
     title: "Web Front End Intern",
     subtitle: "Commencis",
-    description: "During the internship, I developed a Digital Library website that users are able to view, upload, rate, and share digital books, magazines and any other written content. The website was mainly build in React.js with TypeScript. Ant Design was used as a UI framework. Redux was used for state management.",
+    description:
+      "During the internship, I developed a Digital Library website that users are able to view, upload, rate, and share digital books, magazines and any other written content. The website was mainly build in React.js with TypeScript. Ant Design was used as a UI framework. Redux was used for state management.",
     companyLink: "https://www.commencis.com/",
     image: Commencis,
   },
@@ -25,7 +28,8 @@ export const experiences = [
     date: "September 2020 - December 2020",
     title: "ITU ACM Machine Learning Study Group Lead",
     subtitle: "ITU ACM",
-    description: "Taught basic concepts of Machine Learning; numerical optimization, linear regression, logistic regression, and neural networks. Each week, there was a theoretical presentation and implementation of the related topics.",
+    description:
+      "Taught basic concepts of Machine Learning; numerical optimization, linear regression, logistic regression, and neural networks. Each week, there was a theoretical presentation and implementation of the related topics.",
     companyLink: "https://ituacm.com/",
     image: ItuAcm,
   },
@@ -33,16 +37,26 @@ export const experiences = [
     date: "November 2021",
     title: "Deep Learning Study Group Lecturer",
     subtitle: "inzva",
-    description: "Gave a lecture about the optimization algorithms used in Deep Neural Networks (SGD, Momentum, RMSprop, and Adam) and Batch Normalization.",
+    description:
+      "Gave a lecture about the optimization algorithms used in Deep Neural Networks (SGD, Momentum, RMSprop, and Adam) and Batch Normalization.",
     companyLink: "https://inzva.com/",
-    image: Inzva
+    image: Inzva,
   },
   {
-    date: "July 2021 - Present",
+    date: "July 2021 - September 2022",
     title: "Software Engineer",
     subtitle: "DefineX",
-    description: "Implemented a front-end of a website with HTML, CSS, Javascript, and Jquery that serves public APIs of the company. Additionally, wrote unit tests for Javascript functions using Jest. Giving support for development of documentation website that is build using React.js and Spring Boot",
-    companyLink: "",
+    description:
+      "Implemented a front-end of a website with HTML, CSS, Javascript, and Jquery that serves public APIs of the company. Additionally, wrote unit tests for Javascript functions using Jest. Giving support for development of documentation website that is build using React.js and Spring Boot",
+    companyLink: "https://teamdefinex.com/",
     image: Definex,
   },
-]
+  {
+    date: "January 2023",
+    title: "Software Development Engineer I",
+    subtitle: "Amazon Development Center",
+    description: "Bucharest, Romania",
+    companyLink: "http://romania.amazon.com/#!/",
+    image: Amazon,
+  },
+];
